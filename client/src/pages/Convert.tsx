bddf669ec1a4eb586cd39887bfd61b1e3d0a3295
Sklyarenko Aleksandr <63131724+sklyarix@users.noreply.github.com>
2024-07-17 16:50:00 +0300

    upd fast

--- client/src/pages/Convert.tsx
+++ client/src/pages/Convert.tsx
@@ -3,21 +3,9 @@ import { useState } from 'react'
 import { LuArrowDownUp } from 'react-icons/lu'
 import GetCurrency from '../api/GetCurrency.tsx'
 
-import InputConvert from '../ui/Input/InputConvert.tsx'
 import SelectConvert from '../ui/SelectConvert/SelectConvert.tsx'
 
 import styles from './Convert.module.scss'
-
-interface Option {
-  value: string;
-  label: string;
-}
-
-
-
-interface SelectConvertProps {
-  dataCurrency: Coin[];
-}
 
 const Convert = () => {
 	const { data, isLoading, error } = useQuery({
@@ -27,39 +15,32 @@ const Convert = () => {
 
 	if (isLoading) return <div>Loading...</div>
 	if (error) return <div>Error: {(error as Error).message}</div>
-	if (!data) return <div>No data available</div>;
+	if (!data) return <div>No data available</div>
 
-	//const [selectedOption, setSelectedOption] = useState<Option | null>(null)
-	
-	// eslint-disable-next-line react-hooks/rules-of-hooks
 	const [sendValue, setSendValue] = useState(null)
-	// eslint-disable-next-line react-hooks/rules-of-hooks
-	//const [receiveValue, setReceiveValue] = useState(null)
-	
-	
-	
+
+	//console.log('data =', data)
 	return (
 		<div className={styles.wrapper}>
-			
 			<div className={styles.item}>
 				<div className={styles.title}>Send</div>
 				<div className={styles['item-bottom']}>
-					<InputConvert />
-					<SelectConvert data={data} value={sendValue} setValue={setSendValue} />
+					<SelectConvert
+						value={sendValue}
+						setValue={setSendValue}
+						dataCoins={data}
+					/>
 				</div>
 			</div>
 
 			<div className={styles['arrow-swap']}>
 				<LuArrowDownUp />
 			</div>
-			
-			
 		</div>
 	)
 }
 
 export default Convert
-
 
 /*
 <div className={styles.item}>
@@ -69,4 +50,8 @@ export default Convert
 					<SelectConvert data={data} value={receiveValue} setValue={setReceiveValue}/>
 				</div>
 			</div>
+
+
+			//import InputConvert from '../ui/Input/InputConvert.tsx' <InputConvert />
+
 * */
--- client/src/ui/SelectConvert/SelectConvert.tsx
+++ client/src/ui/SelectConvert/SelectConvert.tsx
@@ -1,25 +1,111 @@
-
 import Select from 'react-select'
+import { Coin } from '../../types/Coin.ts'
 
 import styles from './SelectConvert.module.scss'
 
 interface Option {
-  value: string;
-  label: string;
+	value: string
+	label: string
+}
+
+interface Props {
+	value: number
+	setValue: (value: number) => void
+	dataCoins: Coin[]
+}
+
+/*
+type SelectProps = {
+  selected: Option | null;
+  options: Option[];
+  placeholder?: string;
+  mode?: 'rows' | 'cells';
+  status?: 'default' | 'invalid';
+  onChange?: (selected: Option['value']) => void;
+  onClose?: () => void;
+};
+*/
+
+const SelectConvert = ({ value, setValue, dataCoins }: Props) => {
+	const options: Option[] = dataCoins.map(coin => ({
+		value: coin.name,
+		label: coin.slug,
+	}))
+
+	const getValue = () => {
+		return value ? options.find(coinSlug => +)
+	}
+
+	const onChange = (newValue: any) => {
+		setValue(newValue.value)
+	}
+
+	//const changeSelect = setValue(value => value)
+
+	return (
+		<>
+			<Select
+				placeholder={'Select currency'}
+				options={options}
+				className={styles.select}
+				classNamePrefix='currency-select'
+				isSearchable={false}
+				value={getValue}
+				onChange={onChange}
+			/>
+		</>
+	)
+}
+
+export default SelectConvert
+
+/*
+function MyButton() {
+    const [count, setCount] = useState(0);
+
+    function handleClick() {
+        setCount(count + 1);
+    }
+    return (
+        <button
+            onClick={handleClick}
+            className="bg-blue-500 hover:bg-blue-700 rounded py-2 px-4 text-white font-bold"
+        >
+            I have been clicked {count} times
+        </button>
+    );
 }
 
 
-// @ts-ignore
-const SelectConvert = (data, value, setValue) =>  {
-	
+
+
+
+
+
+
+interface Option {
+	value: string
+	label: string
+}
+
+
+/*
+data = { data }
+value = { sendValue }
+setValue = { setSendValue }
+*/
+// data: any, value: any, setValue: any
+
+/*
 	const options: Option[] = data.map((coin) => ({
     value: coin.name,
     label: coin.slug,
   }));
-	
-	return (
-		<>
-			<Select
+	*/
+
+/*
+
+<Select
 				placeholder={dataCurrency[0]?.name || 'Select currency'}
 				options={options}
 				className={styles.select}
@@ -28,8 +114,5 @@ const SelectConvert = (data, value, setValue) =>  {
 				onChange={setSelectedOption}
 				isSearchable={false}
 			/>
-		</>
-	)
-}
-
-export default SelectConvert
+			
+*/
